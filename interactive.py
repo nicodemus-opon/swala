7faba637df1c4ae0506df9d2abada0eb5ef9e66d
Nicodemus Opon <nicodemusopon@gmail.com>
2018-12-29 19:56:17 +0300

    idea

--- interactive.py
+++ interactive.py
@@ -3,37 +3,40 @@ def zipdir(path, ziph):
     for root, dirs, files in os.walk(path):
         for file in files:
             ziph.write(os.path.join(root, file))
-            
+
+
 def create_db(name):
-    nam=name
-    name+=".zip"
+    nam = name
+    name += ".zip"
     zipf = zipfile.ZipFile(name, 'w', zipfile.ZIP_DEFLATED)
     zipdir('temp', zipf)
     zipf.close()
-    hu={"success":"created database "+nam}
+    hu = {"success": "created database " + nam}
     print(hu)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     import flatfile
     import zipfile
     import os
     import json
+
     print("SWALA CLI")
-    xc=""
-    n=0
-    zs=flatfile.flat("nico")
+    xc = ""
+    n = 0
+    zs = flatfile.flat("nico")
     zs.connect()
-    while xc!="quit":
-        xc=input(":"+str(n)+":")
-        xc=xc.lower()
-        x=xc.split(" ")
-        if x[0]=="init":
+    while xc != "quit":
+        xc = input(":" + str(n) + ":")
+        xc = xc.lower()
+        x = xc.split(" ")
+        if x[0] == "init":
             create_db(x[1])
             break
-        n+=1
-        ii=zs.exec(xc)
-        #print(ii)
-        p=str(ii)
-        p=p.replace("'", '"')
+        n += 1
+        ii = zs.exec(xc)
+        # print(ii)
+        p = str(ii)
+        p = p.replace("'", '"')
         parsed = json.loads(p)
         print(json.dumps(parsed, indent=4, sort_keys=True))
