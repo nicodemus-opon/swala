1b226336d360dc0c53f8d034701a7e9e96f9da05
Nicodemus Opon <nicodemusopon@gmail.com>
2019-01-02 15:03:23 +0300

    working

--- condition.py
+++ condition.py
@@ -14,27 +14,36 @@ dd = [
 ]
 
 
+def replace_all(text, keys_, xc):
+    for t in keys_:
+        # print(xc[t])
+        dc = text.split(t)
+        # print(dc)
+        text = xc[t].join(dc)
+    return (text)
+
+
 def check_condition(dd, cond):
-    x = 1
+    y = 1
     keys = list(dd[0].keys())
     keys_ = []
+    correct = []
     for x in keys:
         if x in cond:
             keys_.append(str(x))
-    print(keys_)
+    # print(keys_)
     for xc in dd:
         # print(xc)
-        for t in keys_:
-            print(xc[t])
-            dc = cond.split(t)
-            print(dc)
-            condx = xc[t].join(dc)
-            # cond = cond.replace(t, xc[t])
-        print(condx)
+        condx = replace_all(cond, keys_, xc)
+        # cond = cond.replace(t, xc[t])
+        if (eval(condx)):
+            correct.append(str(y))
+        y += 1
         # print(list(xc.values()))
+    return (correct)
 
 
-print(check_condition(dd, "gender==male and name==jane"))
+print(check_condition(dd, "'gender'=='male' or 'name'=='jane'"))
 '''
 key=io and key=io
 key=io or key=io
